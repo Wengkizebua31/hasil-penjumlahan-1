4d5a55118d72173ec3ae1646a47e12798613789f
Wengkizebua31 <162779370+Wengkizebua31@users.noreply.github.com>
2024-03-09 13:07:52 +0700

    Add files via upload

---
+++ MenghitungPenjumlahan.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package menghitungpenjumlahan;
+
+/**
+ *
+ * @author ELVINDO WIBOWO
+ */
+public class MenghitungPenjumlahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int nilai1, nilai2, jumlah,mod;
+        
+        nilai1=769;
+        
+        
+        nilai2=477;
+        
+        jumlah = nilai1 + nilai2;
+        
+        mod=nilai1 + nilai2;
+        
+        
+        System.out.println("hasil penjumlahan:"+nilai1+" + "+nilai2+"="+jumlah);
+        
+        
+        
+        
+                
+               
+        
+                
+        
+    }  
+    
+    
+    
+}
